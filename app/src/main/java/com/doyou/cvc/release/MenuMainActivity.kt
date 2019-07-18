61ba86a5fff65521d4727513d194a7d1b226f30f
hongbing <hongbinghp@163.com>
2019-07-18 16:02:22 +0800

    新增左侧菜单，用于展示个人相关信息

--- app/src/main/java/com/doyou/cvc/release/MenuMainActivity.kt
+++ app/src/main/java/com/doyou/cvc/release/MenuMainActivity.kt
@@ -2,18 +2,29 @@ package com.doyou.cvc.release
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.doyou.cvc.DispatchManager
 import com.doyou.cvc.R
 import com.doyou.cvc.release.taperchart.ScrollTaperChartActivity
 import com.doyou.cvc.release.taperchart.TaperChartActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.content_menu_main.*
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MenuMainActivity : AppCompatActivity(), View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_main)
         // 避免通过系统安装器打开app，进入到子页面，点击home键导致的启动页重启的问题
         if (!isTaskRoot && intent != null) {
             if (intent.hasCategory(Intent.CATEGORY_LAUNCHER)
@@ -22,7 +33,22 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 return
             }
         }
-        setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "您好，有问题可以右滑查看我的联系方式或在GitHub上提issue哦", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val toggle = ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navView.setNavigationItemSelectedListener(this)
         initListener()
     }
 
@@ -90,4 +116,55 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    override fun onBackPressed() {
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return false // fasle隐藏menu
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_wechat -> {
+                // Handle the camera action
+            }
+            R.id.nav_qq -> {
+
+            }
+//            R.id.nav_slideshow -> {
+//
+//            }
+//            R.id.nav_tools -> {
+//
+//            }
+            R.id.nav_share -> {
+
+            }
+            R.id.nav_send -> {
+
+            }
+        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
 }
--- app/src/main/java/readme.txt
+++ app/src/main/java/readme.txt
@@ -5,3 +5,7 @@ kotlin
 数组转集合：array.toList() as List<T>  |   array.toList() as ArrayList<String>   |   Arrays.asList(names)   |  Arrays.asList("a", "b", "c")
 
 项目支持androidx：https://developer.android.com/jetpack/androidx/migrate#blog_posts
+
+
+Android x 原始支持库 API 软件包映射到 androidx 命名空间：
+* https://developer.android.com/jetpack/androidx/migrate?hl=zh-cn
